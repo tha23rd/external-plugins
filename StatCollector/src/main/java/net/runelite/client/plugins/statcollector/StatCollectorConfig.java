c6f8b032c6779697b89c8d1afc12d9c06ee04267
tha23rd <>
2019-11-10 11:59:34 -0500

    Updating from internal

--- StatCollector/src/main/java/net/runelite/client/plugins/statcollector/StatCollectorConfig.java
+++ StatCollector/src/main/java/net/runelite/client/plugins/statcollector/StatCollectorConfig.java
@@ -1,4 +1,4 @@
-package main.java.net.runelite.client.plugins.statcollector;
+package net.runelite.client.plugins.statcollector;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
